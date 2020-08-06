34bbd5cf0f3ced47cc24581bde178334a8a5035b
chowell2000 <chowell2000@gmail.com>
2020-08-06 15:10:35 -0700

    sample ciphertext

---
+++ applications/histo/histo.py
@@ -0,0 +1,42 @@
+# Your code here
+
+# Use frequency analysis to find the key to ciphertext.txt, and then
+# decode it.
+
+# Your code here
+import operator
+file = open('robin.txt')
+
+s = file.read()
+file.close()
+ignored = ['"',".", "," , ':',';','.','-','+','=','/','|','[',']',"{","}"
+                ,"(", ")","*",'^','&', "\ ", "\\", '!', "'", '-', '—', '?', '1']
+
+def word_count(s):
+    # ignored = ['"',".", "," , ':',';','.','-','+','=','/','|','[',']',"{","}","(", ")","*",'^','&', "\ ", "\\"]
+    for i in ignored:
+        s = s.replace(i,"")
+    words = s.lower().split()
+    count = dict()
+    for word in words:
+        if word in count.keys():
+            count[word] += 1
+        else:
+            count[word] = 1
+
+    # return count
+
+
+    count = {k: v for k,v in reversed(sorted(count.items(), key=operator.itemgetter(0)))}
+    count = {k: v for k,v in reversed(sorted(count.items(), key=operator.itemgetter(1)))}
+    for key in count:
+        graph = 'X'*count[key]
+        print("{:>15} {}".format(key, graph))
+    return count
+
+# txtt = 'The quick brown fox jumped over the lazy turtle'
+
+# print(letter_count(ciphertext))
+# print(len(letter_count(ciphertext)))
+
+print(word_count(s))
